1d3f14b7b83eea9ef0b6e99628c89961e78c5738
nithiyn <vijeasns@amazon.com>
2025-05-01 13:21:58 -0400

    Commit updates

---
+++ fine-tune/scripts/tensorboard.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# Find latest experiment dir
+EXP_DIR=$(ls -dt ./training_repo/examples/nemo_experiments/hf_llama3_8B/* | head -1)
+cd $EXP_DIR
+
+# Launch TensorBoard
+tensorboard --logdir . --port 6006
