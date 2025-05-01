9b23e6c8b212098d5eb920c28f22bd0f3389edd8
nithiyn <vijeasns@amazon.com>
2025-05-01 16:38:08 -0400

    update hardcoded path to ckpt converter

--- fine-tune/scripts/convert_checkpoints.sh
+++ fine-tune/scripts/convert_checkpoints.sh
@@ -4,7 +4,7 @@ set -e
 cd ~/nki-llama/fine-tune/
 
 git clone https://github.com/aws-neuron/neuronx-distributed-training.git
-cd ~/nki-llama/fine-tune/neuronx-distributed-training/examples/
+cd ~/nki-llama/fine-tune/neuronx-distributed-training/examples/checkpoint_converter_scripts
 # Use NxDT’s checkpoint converter to produce sharded NxDT checkpoint
 export PCKPT_MODEL_DIR="~/nki-llama/fine-tune/model_assets/converted_hf_style_hf_to_nxdt_tp8pp4/"
 export MODEL_CONFIG="~/nki-llama/fine-tune/configs/model-config/config.json"
