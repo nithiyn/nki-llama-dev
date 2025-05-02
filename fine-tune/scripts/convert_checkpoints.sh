689dc513cbb3b0fbaf7eab30ae8b9776d385f629
nithiyn <vijeasns@amazon.com>
2025-05-01 22:17:33 -0400

    cm

--- fine-tune/scripts/convert_checkpoints.sh
+++ fine-tune/scripts/convert_checkpoints.sh
@@ -6,9 +6,9 @@ cd ~/nki-llama/fine-tune/
 git clone https://github.com/aws-neuron/neuronx-distributed-training.git
 cd ~/nki-llama/fine-tune/neuronx-distributed-training/examples/checkpoint_converter_scripts
 # Use NxDT’s checkpoint converter to produce sharded NxDT checkpoint
-export PCKPT_MODEL_DIR="~/nki-llama/fine-tune/model_assets/converted_hf_style_hf_to_nxdt_tp8pp4/"
-export MODEL_CONFIG="~/nki-llama/fine-tune/configs/model-config/config.json"
-export BIN_MODEL_DIR="~/nki-llama/fine-tune/model_assets/pckpt/pytorch_model.bin"
+export PCKPT_MODEL_DIR=~/nki-llama/fine-tune/model_assets/converted_hf_style_hf_to_nxdt_tp8pp4/
+export MODEL_CONFIG=~/nki-llama/fine-tune/configs/model-config/config.json
+export BIN_MODEL_DIR=~/nki-llama/fine-tune/model_assets/pckpt/pytorch_model.bin"
 python3 checkpoint_converter.py \
     --model_style             hf \
     --hw_backend              trn1 \
